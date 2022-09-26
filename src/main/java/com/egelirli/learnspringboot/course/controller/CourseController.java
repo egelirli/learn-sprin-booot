3335b41dab2d77cfb9288d1d6c19c61b5b125afe
egelirli <egelirli@yahoo.com>
2022-09-26 20:40:04 +0300

    Section 8 tamamlandı

--- src/main/java/com/egelirli/learnspringboot/course/controller/CourseController.java
+++ src/main/java/com/egelirli/learnspringboot/course/controller/CourseController.java
@@ -20,7 +20,7 @@ public class CourseController {
 
 		@GetMapping("/courses/1")
 		public Course getCourseDetail(){
-			return new Course(1, "Learn SpringBoot", "In28Minutes");
+			return new Course(1, "Learn SpringBoot 1", "In28Minutes");
 			
 		}
 
